6e6ca03d324df4659df0b39e59f35a10675ac227
akannan1087 <15877864+akannan1087@users.noreply.github.com>
2023-05-14 12:27:09 -0500

    triggering jenkins build

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "We are learning Deployment of Java+ Docker + SpringBoot + Helm + EKS Cluster!!!!");
-        model.addAttribute("msg", "Welcome to the springboot docker container running inside EKS Cluster!!!!");
+        model.addAttribute("title", "Hello People, We are deploying  SpringBoot Docker container from Jenkins pipeline using Helm inside EKS Cluster!");
+        model.addAttribute("msg", "Hello People, We are deploying  SpringBoot Docker container from Jenkins pipeline using Helm inside EKS Cluster!!!!!");
         return "index";
     }
 
