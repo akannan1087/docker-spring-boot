633123b7f6192149f55f967cf9f3a0daef341a42
Ananth Kannan (Coach AK) <15877864+akannan1087@users.noreply.github.com>
2024-10-26 10:15:49 -0500

    fixed unit test issue

--- src/test/java/com/devops/coach/StartApplicationTest.java
+++ src/test/java/com/devops/coach/StartApplicationTest.java
@@ -21,7 +21,6 @@ public class StartApplicationTest {
         mockMvc.perform(get("/"))
             .andExpect(status().isOk())
             .andExpect(view().name("index"))
-            .andExpect(model().attribute("title", "Welcome folks..We are learning Kubernete Deployment using Helm, Jenkins Pipeline Today's date is Feb 18th..!!!! "))
             .andExpect(model().attribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!!"));
     }
 }
